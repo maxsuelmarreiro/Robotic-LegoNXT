350a29bb3ab1252ef57d29f80bd78e19297ea55c
Maxsuel Marreiro <maxsuelmarreiro@gmail.com>
2016-06-08 01:21:09 -0300

    Create BTReceive.java

---
+++ src/br/uern/BTReceive.java
@@ -0,0 +1,46 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+import lejos.nxt.LCD;
+import lejos.nxt.comm.BTConnection;
+import lejos.nxt.comm.Bluetooth;
+
+public class BTReceive {
+
+	public static void main(String[] args) throws Exception {
+		String connected = "Connected";
+		String waiting = "Waiting...";
+		String closing = "Closing...";
+
+		while (true) {
+			LCD.drawString(waiting, 0, 0);
+			LCD.refresh();
+
+			BTConnection btc = Bluetooth.waitForConnection();
+
+			LCD.clear();
+			LCD.drawString(connected, 0, 0);
+			LCD.refresh();
+
+			DataInputStream dis = btc.openDataInputStream();
+			DataOutputStream dos = btc.openDataOutputStream();
+
+			for (int i = 0; i < 100; i++) {
+				int n = dis.readInt();
+				LCD.drawInt(n, 7, 0, 1);
+				LCD.refresh();
+				dos.writeInt(-n);
+				dos.flush();
+			}
+
+			dis.close();
+			dos.close();
+			Thread.sleep(100); // wait for data to drain
+			LCD.clear();
+			LCD.drawString(closing, 0, 0);
+			LCD.refresh();
+			btc.close();
+			LCD.clear();
+		}
+	}
+}
